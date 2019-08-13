1b342faf9271828c18e8e91771c35effa882159b
anagha-01 <anaghakrishnan98@gmail.com>
2019-08-13 15:01:57 +0530

    Basic operations (custom modules)

---
+++ nodejs/operation.js
@@ -0,0 +1,16 @@
+module.exports.add=(x,y)=>{
+    var z=x+y
+    return z
+}
+module.exports.sub=(x,y)=>{
+    var sb=x-y
+    return sb
+}
+module.exports.mul=(x,y)=>{
+    var ml=x*y
+    return ml
+}
+module.exports.div=(x,y)=>{
+    var d=x/y
+    return d
+}
--- nodejs/sum.js
+++ nodejs/sum.js
@@ -1,4 +1,11 @@
+const op=require('./operation')
 var a=10
 var b=5
-var s=a+b
+var s=op.add(a,b)
+var s1=op.sub(a,b)
+var m=op.mul(a,b)
+var d=op.div(a,b)
 console.log("sum is "+s)
+console.log("Difference "+s1)
+console.log("Product is "+m)
+console.log("Division "+d)
